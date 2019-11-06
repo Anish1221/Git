2d9745ba080ec80102271ec03c90ded1ac830ced
naren <naren@nepcert.com>
2019-11-06 11:21:42 +0545

    test

--- GitTutorial/src/com/example/git/Test.java
+++ GitTutorial/src/com/example/git/Test.java
@@ -3,9 +3,13 @@ package com.example.git;
 public class Test {
 
 	public static void main(String[] args) {
+		int sum =0;
 		for(int i=0;i<10;i++){
 			System.out.println(i);
+			sum +=i;
+			
 		}
+		System.out.println(sum);
 	}
 	
 }
