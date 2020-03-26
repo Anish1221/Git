e363023fedde9a774d60756e329a940aa1f7f351
anish <anishtwayana02@gmail.com>
2020-03-26 15:08:00 +0545

    added input parameters

--- GitTutorial/src/com/example/git/Test.java
+++ GitTutorial/src/com/example/git/Test.java
@@ -3,7 +3,7 @@ package com.example.git;
 public class Test {
 
 	public static void main(String[] args) {
-		int[] input = { 10, 9 };
+		int[] input = { 10, 9, 8, 7, 6 };
 		int sum = 0;
 		for (int i : input) {
 			System.out.println(i);
